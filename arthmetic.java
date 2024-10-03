07750d89dc1446dbc1a4fea4ace8d97cbcbe9196
anbusezhiyan s <mod22cud12122@gmail.com>
2024-10-03 12:57:52 +0530

    Add files via upload

---
+++ arthmetic.java
@@ -0,0 +1,12 @@
+public class arthmetic{
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));  
+        System.out.println("a - b = " + (a - b));  
+        System.out.println("a * b = " + (a * b));  
+        System.out.println("b / a = " + (b / a));  
+        System.out.println("b % a = " + (b % a));   
+    }
+}
